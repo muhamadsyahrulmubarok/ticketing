1d723f0eb6d863f2d9a13bba421d89189fe22f9e
Syahrul Mubarok <muhamadsyahrulmubarok@gmail.com>
2023-02-15 17:15:54 +0700

    update

--- application/views/header/header.php
+++ application/views/header/header.php
@@ -1,10 +1,28 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Help Desk - Pertamina</title>
+<title>Help Desk - BPR Nusumma Jateng</title>
 <?php
 setlocale(LC_ALL, 'id_ID.UTF8', 'id_ID.UTF-8', 'id_ID.8859-1', 'id_ID', 'IND.UTF8', 'IND.UTF-8', 'IND.8859-1', 'IND', 'Indonesian.UTF8', 'Indonesian.UTF-8', 'Indonesian.8859-1', 'Indonesian', 'Indonesia', 'id', 'ID', 'en_US.UTF8', 'en_US.UTF-8', 'en_US.8859-1', 'en_US', 'American', 'ENG', 'English');
 
 ?>
+<link rel="apple-touch-icon" sizes="57x57" href="<?= base_url(); ?>/asset/image/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?= base_url(); ?>/asset/image/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?= base_url(); ?>/asset/image/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?= base_url(); ?>/asset/image/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?= base_url(); ?>/asset/image/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?= base_url(); ?>/asset/image/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?= base_url(); ?>/asset/image/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?= base_url(); ?>/asset/image/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?= base_url(); ?>/asset/image/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192" href="<?= base_url(); ?>/asset/image/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?= base_url(); ?>/asset/image/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?= base_url(); ?>/asset/image/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?= base_url(); ?>/asset/image/favicon-16x16.png">
+<link rel="manifest" href="<?= base_url(); ?>/asset/image/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="<?= base_url(); ?>/asset/image/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
 <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
 <link href="<?php echo base_url(); ?>assets/css/datepicker3.css" rel="stylesheet">
 <link href="<?php echo base_url(); ?>assets/css/bootstrap-table.css" rel="stylesheet">
